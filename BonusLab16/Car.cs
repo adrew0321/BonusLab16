e0808daa9bc77d071c6c1de6b7a4efb016b07203
A'Keem Drew <adrew0321@gmail.com>
2018-07-31 07:44:29 -0400

    First commit/Created Car Class

---
+++ BonusLab16/Car.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BonusLab16
+{
+    class Car
+    {
+        //Fields to represent data (private)
+
+        #region Fields
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+        #endregion Fields
+
+        // Properties
+        #region Properties
+        public string Make
+        {
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z]{15}$"))
+                {
+                    throw new Exception("Make is not a valid input");
+                }
+                else
+                {
+                    make = value;
+                }
+            }
+
+            get { return Make; }
+        }
+
+        public string Model
+        {
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-Z]{15}$"))
+                {
+                    throw new Exception("Model is not a valid input");
+                }
+                else
+                {
+                    model = value;
+                }
+            }
+
+            get { return Model; }
+        }
+
+        public int Year
+        {
+            set
+            {
+                if (!Regex.IsMatch(value.ToString(), @"^\d[1-9]{1,4}$"))
+                {
+                    throw new Exception("The year entered is not a valid input");
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+
+            get { return year; }
+        }
+
+        public double Price
+        {
+            set
+            {
+                if (!Regex.IsMatch(value.ToString(), @"^\d{1,9}.\d{1,2}$"))
+                {
+                    throw new Exception("The price entered is not a valid");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+
+            get { return price; }
+
+        }
+        #endregion Properties
+    }
+}
+
+
+
+
---
+++ BonusLab16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BonusLab16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ***INPUT***
+            Console.WriteLine("\nWelcome to the Grand Circus Motors admin console!");
+
+            Console.Write("\nHow many cars are you entering: ");
+
+
+
+
+
+
+            // ***PROCESSING
+
+
+
+
+
+
+
+
+            // ***OUTPUT***
+            //Console.WriteLine("Current Inventory: ");
+
+
+
+        }
+    }
+}
